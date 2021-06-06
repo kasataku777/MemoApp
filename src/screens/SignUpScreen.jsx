079cb1435d75c120cea28b52f9373f70d828b4b8
kasataku777 <takumi.jihen@gmail.com>
2021-06-07 05:17:16 +0900

    implement signup and login

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import {View,Text,StyleSheet,TextInput, Alert,TouchableOpacity} from 'react-native';
-
+import firebase from 'firebase';
 
 import Button from '../components/Button';
 
@@ -8,6 +8,23 @@ export default function SignUpScreen(props){
     const {navigation} = props;
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+
+    function handlePress(){
+        firebase.auth().createUserWithEmailAndPassword(email,password)
+        .then((userCredential)=>{
+            const {user}=userCredential;
+            console.log(user.uid);
+            navigation.reset({
+                index:0,
+                routes:[{name:'MemoList'}],
+            });
+        }).catch(
+            (error)=>{
+                console.log(error.code,error.message);
+                Alert.alert(error.code);
+            }
+        );
+    }
 
     return (
         <View style={styles.container}>
@@ -32,11 +49,7 @@ export default function SignUpScreen(props){
                 secureTextEntry
                 textContentType="password"
                 />
-                <Button label="Submit" onPress={()=>{navigation.reset({
-                    index:0,
-                    routes:[{name:'MemoList'}],
-                })
-                }}/>
+                <Button label="Submit" onPress={handlePress}/>
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
                     <TouchableOpacity onPress={()=>{navigation.reset({
