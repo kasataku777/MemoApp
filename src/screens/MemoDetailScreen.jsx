e0e5aaed2dd773d12d5782a284699acc8147f6ef
kasataku777 <takumi.jihen@gmail.com>
2021-06-06 17:44:59 +0900

    connect screens with react-navigation

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {View,ScrollView,Text,StyleSheet} from 'react-native';
 
-import AppBar from '../components/AppBar';
+
 import CircleButton from '../components/CircleButton';
 
-export default function MemoDetailScreen(){
+export default function MemoDetailScreen(props){
+    const {navigation}=props;
     return (
         <View style={styles.container}>
-            <AppBar/>
+
             <View style={styles.memoHeader}>
                 <Text style={styles.memoTitle}>買い物リスト</Text>
                 <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
@@ -20,7 +21,10 @@ export default function MemoDetailScreen(){
 
                 </Text>
             </ScrollView>
-            <CircleButton style={{top:160,bottom:'auto'}} name="pencil"/>
+            <CircleButton
+            style={{top:60,bottom:'auto'}}
+            name="pencil"
+            onPress={()=>{navigation.navigate('MemoEdit')}}/>
         </View>
     )
 }
