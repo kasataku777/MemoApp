f982c9aaac5b4fac2a952050e9c9422d791209de
kasataku777 <takumi.jihen@gmail.com>
2021-06-09 08:19:15 +0900

    create loading component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -2,11 +2,13 @@ import React,{useState,useEffect} from 'react';
 import {View,Text,StyleSheet,TextInput,TouchableOpacity, Alert} from 'react-native';
 import firebase from 'firebase';
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props){
     const {navigation} = props;
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const [isLoading,setLoading] = useState(true);
 
     // useEffect(()=>{
     //     console.log('useEffect');
@@ -24,12 +26,15 @@ export default function LogInScreen(props){
                     index:0,
                     routes:[{name:'MemoList'}],
                 });
+            }else{
+                setLoading(false);
             }
         });
         return unsubscribe;
     },[]);
 
     function handlePress(){
+        setLoading(true);
         firebase.auth().signInWithEmailAndPassword(email,password)
         .then(
             (userCredential)=>{
@@ -44,11 +49,14 @@ export default function LogInScreen(props){
             (error)=>{
                 Alert.alert(error.code);
             }
-        )
+        ).then(()=>{
+            setLoading(false);
+        })
 
     }
     return (
         <View style={styles.container}>
+            <Loading isLoading={isLoading} />
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
                 <TextInput
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,13 +1,18 @@
 import React,{useEffect,useState} from 'react';
-import {View,StyleSheet, Alert} from 'react-native';
+import {View,StyleSheet, Alert,Text} from 'react-native';
 import firebase from 'firebase';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
+import Button from '../components/Button';
+import Loading from '../components/Loading';
+
+
 export default function MemoListScreen(props){
   const {navigation} = props;
   const [memos,setMemos]=useState([]);
+  const [isLoading,setLoading]=useState(false);
 
   useEffect(()=>{
     navigation.setOptions({
@@ -20,6 +25,7 @@ export default function MemoListScreen(props){
     const {currentUser} = firebase.auth();
     let unsubscribe=()=>{};
     if(currentUser){
+      setLoading(true);
       const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt','desc');
       unsubscribe=ref.onSnapshot((snapshot)=>{
         const userMemos=[];
@@ -33,17 +39,31 @@ export default function MemoListScreen(props){
           });
         });
         setMemos(userMemos);
+        setLoading(false);
       },(error)=>{
         console.log(error);
+        setLoading(false);
         Alert.alert('データの読み込みに失敗しました')
       });
     }
     return unsubscribe;
   },[]);
 
+  if(memos.length===0){
+    return (
+      <View style={emptyStyles.container}>
+         <Loading isLoading={isLoading}/>
+        <View style={emptyStyles.inner}>
+        <Text style={emptyStyles.title}>最初のメモを作成しよう！</Text>
+        <Button style={emptyStyles.button}label="作成する" onPress={()=>{navigation.navigate('MemoCreate');}}/>
+      </View>
+      </View>
+
+    )
+  }
+
     return (
         <View style={styles.container}>
-
 
         <MemoList memos={memos}/>
 
@@ -62,3 +82,25 @@ const styles = StyleSheet.create({
     },
 
   });
+
+  const emptyStyles=StyleSheet.create(
+    {
+      container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+      },
+      inner:{
+        justifyContent:'center',
+        alignItems:'center'
+      },
+      title:{
+        fontSize:18,
+        marginBottom:24
+      },
+      button:{
+        alignSelf:'center'
+      }
+
+    }
+  )
